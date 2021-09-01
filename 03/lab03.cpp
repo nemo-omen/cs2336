75cb61db3f834b23e84cd022c50b94505116b741
nemo-omen <nemo.omen@gmail.com>
2021-09-01 07:44:01 -0500

    Get file to class lecture state

--- 03/lab03.cpp
+++ 03/lab03.cpp
@@ -18,36 +18,45 @@ Rational Rational::add(const Rational& addend) const {
     sum.reduce();
 
     // return local object
-
     return sum;
 }
 
 Rational Rational::additiveInverse() const {
-
+    return Rational(this->getNumerator() * -1, this->getDenominator());
 }
 
 Rational Rational::subtract(const Rational& subtrahend) const {
-
+    return this->add(subtrahend.additiveInverse());
 }
 
 Rational Rational::multiply(const Rational& multiplicand) const {
-
+    return Rational();
 }
 
 Rational Rational::multiplicativeInverse() const {
-
+    return Rational();
 }
 
 Rational Rational::divide(const Rational& divisor) const {
-
+    return Rational();
 }
 
 ostream& Rational::print(ostream& os) const {
-
+    os << this->getNumerator() << '/' << this->getDenominator();
+    return os;
 }
 
 istream& Rational::read(istream& is) {
+    int num, denom;
 
+    if(is >> num >> denom) {
+        this->setNumerator(num);
+        this->setDenominator(denom);
+    } else {
+        this->setNumerator(0);
+        this->setDenominator(1);
+    }
+    return is;
 }
 
 int Rational::lcm(int m, int n) const {                         // lcm(m, n) = (m * n) / gcd(m, n)
