ea7aad76f4b246fcaca279889837490b836cfddc
nemo-omen <nemo.omen@gmail.com>
2021-09-21 06:45:44 -0500

    Organize notes by module/start lab 11

---
+++ 11/lab11.cpp
@@ -0,0 +1,23 @@
+// Jeff Caldwell
+// CS 2336
+// Lab 11
+
+#include <lab11.h>
+
+// Test for equality
+bool IntegerSet::operator==(const IntegerSet& rhs) const {
+  bool isEqual = false;
+  return isEqual;
+}
+
+// Subset
+bool IntegerSet::operator<=(const IntegerSet& rhs) const {
+  bool isLessOrEqual = false;
+  return isLessOrEqual;
+}
+
+// Proper Subset
+bool IntegerSet::operator< (const IntegerSet& rhs) const {
+  bool isLessThan = false;
+  return isLessThan;
+}
