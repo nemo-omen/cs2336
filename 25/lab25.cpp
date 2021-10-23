e380c41b4dfce8eeb9428061c733c0f41d78d50d
nemo-omen <nemo.omen@gmail.com>
2021-10-22 20:15:31 -0500

    Update lab 25

---
+++ 25/lab25.cpp
@@ -0,0 +1,27 @@
+// Jeff Caldwell
+// CS 2336
+// Lab 25
+
+template<typename U>
+ostream& operator<<(ostream& output, const Accumulator<U>& obj) {
+  output << *obj.getTotal();
+  return output; 
+}
+
+// constructor
+template <typename T>
+Accumulator<T>::Accumulator(const T& value) {
+  this->total = value;
+}
+
+// return total
+template <typename T>
+T Accumulator<T>::getTotal() const {
+ return this->total;
+}
+
+// add (+) value to total
+template <typename T>
+void Accumulator<T>::addValue(const T& value) {
+  this->total += value;
+}        
