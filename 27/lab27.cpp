41802f03386cac4d89d8d4074e266c8af2deab34
nemo-omen <nemo.omen@gmail.com>
2021-10-28 20:26:05 -0500

    Start lab27

--- 27/lab27.cpp
+++ 27/lab27.cpp
@@ -7,12 +7,27 @@ pair<T, int> modeUsingIndexing(const vector<T>& v) {
   sort(copy.begin(), copy.end());     // O(n log n)
 
   pair<T, int> myMode;
+  T tempMode;
+  int tempCount = 1;
   typename vector<T>::size_type i;
 
   myMode.first = copy[0];
   myMode.second = 1;
-  
-  for(i = 0; i < copy.size(); ++i) {  // O(n)
+
+  tempMode = copy[0];
+
+  for(i = 1; i < copy.size(); ++i) {  // O(n)
+    if(copy[i] == tempMode) {
+      ++tempCount;
+    } else {
+      tempCount = 1;
+      tempMode = copy[i];
+    }
+
+    if(tempCount > myMode.second) {
+      myMode.second = tempCount;
+      myMode.first = tempMode;
+    }
 
   }
 
@@ -25,13 +40,27 @@ pair<T, int> modeUsingIterators(const vector<T>& v) {
   sort(copy.begin(), copy.end());     // O(n log n)
 
   pair<T, int> myMode;
+  T tempMode;
+  int tempCount = 1;
   typename vector<T>::iterator it;
 
-  myMode.first = *(copy.begin());
+  myMode.first = copy[0];
   myMode.second = 1;
-  
-  for(it = copy.begin(); it < copy.end(); ++it) {  // O(n)
 
+  tempMode = copy[0];
+  
+  for(it = copy.begin() + 1; it < copy.end(); ++it) {  // O(n)
+    if(*it == tempMode) {
+      ++tempCount;
+    } else {
+      tempCount = 1;
+      tempMode = *it;
+    }
+
+    if(tempCount > myMode.second) {
+      myMode.second = tempCount;
+      myMode.first = tempMode;
+    }
   }
 
   return myMode;
@@ -43,13 +72,27 @@ pair<T, int> modeUsingPointers(const vector<T>& v) {
   sort(copy.begin(), copy.end());     // O(n log n)
 
   pair<T, int> myMode;
+  T tempMode;
+  int tempCount = 1;
   typename vector<T>::pointer ptr;
 
-  myMode.first = *(copy.data());
+  myMode.first = copy[0];
   myMode.second = 1;
-  
-  for(ptr = copy.data(); ptr < copy.data() + copy.size(); ++ptr) {  // O(n)
 
+  tempMode = copy[0];
+  
+  for(ptr = copy.data() + 1; ptr < copy.data() + copy.size(); ++ptr) {  // O(n)
+    if(*ptr == tempMode) {
+      ++tempCount;
+    } else {
+      tempCount = 1;
+      tempMode = *ptr;
+    }
+
+    if(tempCount > myMode.second) {
+      myMode.second = tempCount;
+      myMode.first = tempMode;
+    }
   }
 
   return myMode;
