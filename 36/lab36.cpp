b7cd51d9bba29ce295aa60fbc99d4c1563e45cfe
trainingmontage <nemo.omen@gmail.com>
2021-11-11 10:46:07 -0600

    Start lab 36

--- 36/lab36.cpp
+++ 36/lab36.cpp
@@ -1,17 +1,22 @@
 // Jeff Caldwell
 // CS 2336
 // Lab 36
+#include <lab36.h>
 
 // 4 loops
 
-
 ostream& operator<<(ostream& out, const Spiral& spiral) {
-  
+  for(int i = 0; i < spiral.data.rows(); i++) {
+    for(int j = 0; j < spiral.data.cols(); j++) {
+      out << spiral.data[i][j] << " ";
+    }
+    out << '\n';
+  }
   return out;
 }
 
 void Spiral::initialize(int n) {
-
+  data.resize(n, n);
 }
 
 void Spiral::build() {
