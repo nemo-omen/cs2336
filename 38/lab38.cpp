3139bcba9b8827843f07d7eafd3ce34a00978d4d
nemo-omen <nemo.omen@gmail.com>
2021-11-17 18:14:40 -0600

    Getting there

--- 38/lab38.cpp
+++ 38/lab38.cpp
@@ -14,60 +14,32 @@ ostream& operator<<(ostream& os, const vector<uint>& v);
 
 void bucketSort(vector<uint>& v, uint numDigits) {
   vector<vector<uint>> buckets (10);
+  vector<uint> out;
   int multiplier = 10;
-  int digitCount = numDigits;
+  // int digitCount = numDigits;
   cout << "before: " << v;
+  vector<uint>::size_type i, j;
 
-  for(int i = 0; i < (int)v.size(); i++) {
+  for(i = 0; i < v.size(); i++) {
     int current = v[i];
     int targetDigit;
 
-    targetDigit = current % 10;
+    targetDigit = current % multiplier;
     buckets[targetDigit].push_back(v[i]);
     --numDigits;
   }
 
-  for(int i = 0; i < (int)buckets.size(); i++) {
+  for(i = 0; i < buckets.size(); i++) {
     cout << "bucket: " << buckets[i];
   }
 
-  for(int i = 0; i < buckets.size(); i++) {
+  for(i = 0; i < buckets.size(); i++) {
     if(buckets[i].size() > 0) {
-      
+      for(j = 0; j < buckets[i].size(); j++) {
+        out.push_back(buckets[i][j]);
+      }
     }
   }
 
-  cout << "buckets after: " << buckets;
-}
-
-vector<uint> merge(vector<uint> &a, vector<uint> &b) {
-  // int size = a.size() > b.size() ? a.size() : b.size();
-  vector<uint> result(a.size() + b.size());
-  vector<unsigned int>::size_type i = 0, j = 0, k = 0;
-
-  while(i < a.size() && j < b.size()) {
-    if(a[i] < b[i]) {
-      result[k] = a[i];
-      ++k;
-      ++i;
-    } else {
-      result[k] = b[j];
-      ++k;
-      ++j;
-    }
-  }
-
-  while(i < a.size()) {
-    result[k] = a[i];
-    ++k;
-    ++i;
-  }
-
-  while(j < b.size()) {
-    result[k] = b[j];
-    ++k;
-    ++j;
-  }
-
-  return result;
+  cout << "out: " << out;
 }
