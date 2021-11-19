db7f45c9b85cf28c4d6bc327c06bf8e7516c9e1a
trainingmontage <nemo.omen@gmail.com>
2021-11-19 16:26:33 -0600

    Start lab 39

--- 39/lab39.cpp
+++ 39/lab39.cpp
@@ -9,14 +9,48 @@ using namespace std;
 
 
 bool isPalindrome(string s) {
-  cout << "before: " << s << '\n';
-  
   stack<char, vector<char>> a, b,c;
+  stack<char, vector<char>>::size_type i, j;
+  // basic_string<char>::size_type k;
+  
+  // cout << "before: " << s << '\n';
   
   transform(s.begin(), s.end(), s.begin(),
     [] (unsigned char c) -> unsigned char { return toupper(c); });
 
-  cout << "after: " << s << '\n';
+  cout << "\nafter: " << s << '\n';
+
+  cout << "\n s: ";
+
+  for(int k = 0; k < s.length(); k++) {
+    cout << s.at(k) << " ";
+    a.push(s.at(k));
+    b.push(s.at(k));
+  }
+
+  cout << '\n' << "b: ";
+  
+  for(i = 0; i < b.size(); i++) {
+    cout << b.top() << " ";
+    c.push(b.top());
+    b.pop();
+  }
+  
+  cout << '\n';
+
+  // for(j = 0; j < a.size(); j++) {
+  //   if(c.top() != a.top()) {
+  //     return false;
+  //   }
+
+  //   cout << "a: " << c.top() << " ";
+  //   cout << "c: " << a.top() << " ";
+
+  //   c.pop();
+  //   a.pop();
+  // }
+
+  cout << '\n';
 
   return true;
 }
